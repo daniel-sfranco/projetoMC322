7bc67845d1322139ef980c1bb9d7fdebdeed017e
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-26 20:42:09 -0300

    Lembrete para mais tarde

--- app/src/main/api/Request.java
+++ app/src/main/api/Request.java
@@ -5,6 +5,7 @@ package api;
  */
 public class Request {
     private String baseUrl = "https://api.spotify.com/";
+    // Eu juro que vou fazer isso ser mais seguro
     private String clientId = "9afeb5fec9854592994aa191f842b529";
     private String clientSecret = "0e4def4ee8924cb68daba80833c8a5c2";
 }

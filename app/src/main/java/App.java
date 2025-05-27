9f54ae9c282465e3c23735223d6fa36f75bc4732
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-27 10:12:27 -0300

    Resolvendo requisições GET e POST em HttpClientUtil, e inserindo caso de uso no App.java

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import api.HttpClientUtil;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,26 +9,35 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+
 public class App extends Application {
     public static void main(String[] args) {
         launch(args);
         System.out.println("Application started");
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-type", "application/json; charset=UTF-8");
+                try {
+                    HttpClientUtil.sendGetRequest("https://jsonplaceholder.typicode.com/posts", headers);
+                    System.out.println("Requisição get enviada com sucesso");
+                    HttpClientUtil.sendPostRequest("https://jsonplaceholder.typicode.com/posts", headers, headers);
+                    System.out.println("Requisição post enviada com sucesso");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
--- app/src/main/java/api/HttpClientUtil.java
+++ app/src/main/java/api/HttpClientUtil.java
@@ -5,7 +5,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,18 +18,33 @@ public class HttpClientUtil {
     private static final HttpClient client = HttpClient.newBuilder().build();
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    public String sendGetRequest(String url, Map<String, String> headers) throws IOException, JsonProcessingException, RequestException, InterruptedException{
+    public static String sendGetRequest(String url, Map<String, String> headers) throws IOException, JsonProcessingException, RequestException, InterruptedException{
         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url)).GET();
         if(headers != null) {
             headers.forEach(requestBuilder::header);
         }
         HttpRequest request = requestBuilder.build();
-        HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
-        String jsonBody = mapper.writeValueAsString(response.body());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         if(response.statusCode() >= 400){
-            throw new RequestException((int) response.statusCode(), jsonBody);
+            throw new RequestException((int) response.statusCode(), response.body());
         }
-        return jsonBody;
+        return response.body();
+    }
+
+    public static String sendPostRequest(String url, Map<String, String> headers, Object bodyObject) throws IOException, InterruptedException, RequestException{
+        String requestBodyJson = mapper.writeValueAsString(bodyObject);
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url)).POST(HttpRequest.BodyPublishers.ofString(requestBodyJson));
+        if(headers != null){
+            headers.forEach(requestBuilder::header);
+        }
+        HttpRequest request = requestBuilder.build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+        if(response.statusCode() >= 400){
+            throw new RequestException((int) response.statusCode(), response.body());
+        }
+        return response.body();
     }
 }
 

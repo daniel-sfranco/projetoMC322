4840b1940a3995d85b923e3d53e41a3069edc80c
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-28 11:56:30 -0300

    Evitando problemas posteriores de merge, voltando para a versão anterior do arquivo App.java, e adicionando um método Main em HttpClientUtil.java para exemplificar o uso da classe.

--- app/src/main/java/api/HttpClientUtil.java
+++ app/src/main/java/api/HttpClientUtil.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -18,7 +19,8 @@ public class HttpClientUtil {
 
     public static String sendGetRequest(String url, Map<String, String> headers)
             throws IOException, JsonProcessingException, RequestException, InterruptedException {
-        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url)).GET();
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url));
+        requestBuilder.GET();
         if (headers != null) {
             headers.forEach(requestBuilder::header);
         }
@@ -33,8 +35,8 @@ public class HttpClientUtil {
     public static String sendPostRequest(String url, Map<String, String> headers, Object bodyObject)
             throws IOException, InterruptedException, RequestException {
         String requestBodyJson = JsonUtil.parseObjectToJson(bodyObject);
-        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url))
-                .POST(HttpRequest.BodyPublishers.ofString(requestBodyJson));
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(url));
+        requestBuilder.POST(HttpRequest.BodyPublishers.ofString(requestBodyJson));
         if (headers != null) {
             headers.forEach(requestBuilder::header);
         }
@@ -46,5 +48,18 @@ public class HttpClientUtil {
             throw new RequestException((int) response.statusCode(), response.body());
         }
         return response.body();
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-type", "application/json; charset=UTF-8");
+        try {
+            HttpClientUtil.sendGetRequest("https://jsonplaceholder.typicode.com/posts", headers);
+            System.out.println("Requisição get enviada com sucesso");
+            HttpClientUtil.sendPostRequest("https://jsonplaceholder.typicode.com/posts", headers, headers);
+            System.out.println("Requisição post enviada com sucesso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

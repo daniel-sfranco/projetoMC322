cb9a3926cf555f2350d70b46606d3b3d27c8f420
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-28 12:36:02 -0300

    Adicionar getter para a url base no request e mostrar caso de uso do JsonUtil.java

--- app/src/main/java/api/JsonUtil.java
+++ app/src/main/java/api/JsonUtil.java
@@ -1,5 +1,7 @@
 package api;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,17 +11,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class JsonUtil {
     private static final ObjectMapper mapper = new ObjectMapper();
-    public static <T> T parseJson(String json, Class<T> valueType)
-            throws JsonMappingException, JsonProcessingException {
-        return mapper.readValue(json, valueType);
-    }
 
-    public static String parseObjectToJson(Object object) throws JsonProcessingException {
+    public static String parseObjectToJson(Object object) {
         try {
             return mapper.writeValueAsString(object);
         } catch (Exception e) {
             System.out.println("Erro ao converter o objeto para JSON: " + e.getMessage());
             return null;
         }
+    }
+
+    public static <T> T parseJson(String json, Class<T> valueType) {
+        try {
+            return mapper.readValue(json, valueType);
+        } catch (JsonMappingException e) {
+            System.out.println("Erro de mapeamento JSON: " + e.getMessage());
+        } catch (JsonProcessingException e) {
+            System.out.println("Erro ao processar JSON: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro desconhecido: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String jsonRequest = parseObjectToJson(new Request());
+        System.out.println(jsonRequest);
+		Request parsedRequest = parseJson(jsonRequest, Request.class);
+        System.out.println(parsedRequest.toString());
     }
 }

11d66ef4b03a76596ffa65c8c0df4c9b746c5f58
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-28 12:36:49 -0300

    Remover imports desnecessários

--- app/src/main/java/api/JsonUtil.java
+++ app/src/main/java/api/JsonUtil.java
@@ -1,7 +1,5 @@
 package api;
 
-import java.util.ArrayList;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;

de3d9c9703b2c848a157e4b0f3fef77133673bf6
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-28 19:38:40 -0300

    Documentação e formatação

--- app/src/main/java/api/Request.java
+++ app/src/main/java/api/Request.java
@@ -50,17 +50,34 @@ public class Request {
     private String clientSecret = "0e4def4ee8924cb68daba80833c8a5c2";
     private SpotifyToken token;
 
+    /**
+     * Retorna a url base da API do Spotify.
+     * 
+     * @return A URL base da API do Spotify.
+     */
     public String getBaseUrl() {
         return baseUrl;
     }
 
+    /**
+     * Retorna o token de autenticação utilizado para as requisições.
+     * 
+     * @return o token.
+     */
     public SpotifyToken getToken() {
         return token;
     }
 
+    /**
+     * Solicita um token de autenticação da API do Spotify.
+     * Utiliza o clientId e clientSecret para obter o token.
+     * 
+     * @return O token de autenticação ou null em caso de falha.
+     */
     public String requestToken() {
         try {
-            String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId, clientSecret);
+            String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId,
+                    clientSecret);
             Map<String, String> headers = Map.of(
                     "Content-Type", "application/x-www-form-urlencoded");
 

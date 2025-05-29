ed4774003bac43a4e8a11a7a85e37266536d0497
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-05-29 14:06:52 -0300

    Trocando a lógica de requisição do token para a classe SpotifyToken, dando maior independência para ela e diminuindo a interface da classe Request

--- app/src/main/java/api/Request.java
+++ app/src/main/java/api/Request.java
@@ -1,6 +1,12 @@
 package api;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import exceptions.RequestException;
 
 /**
  * SpotifyToken
@@ -11,6 +17,13 @@ class SpotifyToken {
     private String access_token;
     private String token_type;
     private int expires_in;
+    private LocalDateTime updatedAt;
+    private String clientId = "9afeb5fec9854592994aa191f842b529";
+    private String clientSecret = "0e4def4ee8924cb68daba80833c8a5c2"; // Eu juro que vou fazer isso ser mais seguro
+
+    public SpotifyToken() throws RequestException {
+        this.refreshToken();
+    }
 
     /**
      * Retorna o token de acesso.
@@ -18,6 +31,15 @@ class SpotifyToken {
      * @return O token de acesso.
      */
     public String getAccess_token() {
+        LocalDateTime now = LocalDateTime.now();
+        if (updatedAt == null || updatedAt.plusSeconds(expires_in).isBefore(now)) {
+            try {
+                return this.refreshToken();
+            } catch (Exception e) {
+                System.out.println("Erro ao atualizar o token de autenticação: " + e.getMessage());
+                return null;
+            }
+        }
         return access_token;
     }
 
@@ -38,6 +60,32 @@ class SpotifyToken {
     public int getExpires_in() {
         return expires_in;
     }
+
+    /**
+     * Atualiza o token de autenticação.
+     * Faz uma requisição para a API do Spotify para obter um novo token.
+     * 
+     * @return O novo token de acesso ou null em caso de falha.
+     */
+    public String refreshToken() throws RequestException {
+        String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId,
+                clientSecret);
+        Map<String, String> headers = Map.of(
+                "Content-Type", "application/x-www-form-urlencoded");
+        String response;
+        response = HttpClientUtil.sendPostFormRequest(
+                "https://accounts.spotify.com/api/token",
+                headers,
+                body);
+        Map<String, String> params = (Map<String, String>) JsonUtil.parseJson(response,
+                new TypeReference<HashMap<String, String>>() {
+                });
+        this.access_token = params.get("access_token");
+        this.token_type = params.get("token_type");
+        this.expires_in = Integer.parseInt(params.get("expires_in"));
+        this.updatedAt = LocalDateTime.now();
+        return access_token;
+    }
 }
 
 /**
@@ -47,11 +95,12 @@ class SpotifyToken {
  * as conexões com a API.
  */
 public class Request {
-    private String baseUrl = "https://api.spotify.com/";
-    // Eu juro que vou fazer isso ser mais seguro
-    private String clientId = "9afeb5fec9854592994aa191f842b529";
-    private String clientSecret = "0e4def4ee8924cb68daba80833c8a5c2";
+    private static String baseUrl = "https://api.spotify.com/v1/";
     private SpotifyToken token;
+
+    public Request() throws RequestException {
+        this.token = new SpotifyToken();
+    }
 
     /**
      * Retorna a url base da API do Spotify.
@@ -71,44 +120,37 @@ public class Request {
         return token;
     }
 
-    /**
-     * Solicita um token de autenticação da API do Spotify.
-     * Utiliza o clientId e clientSecret para obter o token.
-     * 
-     * @return O token de autenticação ou null em caso de falha.
-     */
-    public String requestToken() {
-        try {
-            String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId,
-                    clientSecret);
-            Map<String, String> headers = Map.of(
-                    "Content-Type", "application/x-www-form-urlencoded");
+    public String sendPostRequest(String url, Object bodyObject) throws RequestException {
+        String accessToken = this.token.getAccess_token();
+        Map<String, String> headers = Map.of(
+                "Authorization", "Bearer " + accessToken,
+                "Content-Type", "application/json");
+        return HttpClientUtil.sendPostRequest(url, headers, bodyObject);
+    }
 
-            String response = HttpClientUtil.sendPostFormRequest(
-                    "https://accounts.spotify.com/api/token",
-                    headers,
-                    body);
-            SpotifyToken token = JsonUtil.parseJson(response, SpotifyToken.class);
-            this.token = token;
-            return token.getAccess_token();
-        } catch (Exception e) {
-            System.out.println("Erro ao obter token de autenticação: " + e.getMessage());
-            return null;
-        }
+    public String sendGetRequest(String url) throws RequestException {
+        String accessToken = this.token.getAccess_token();
+        Map<String, String> headers = Map.of(
+                "Authorization", "Bearer " + accessToken,
+                "Content-Type", "application/json");
+        return HttpClientUtil.sendGetRequest(url, headers);
     }
 
     /**
      * Método principal para testar a obtenção do token de autenticação.
      * 
      * @param args Argumentos da linha de comando (não utilizados).
+     * @throws RequestException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RequestException {
         Request request = new Request();
-        String token = request.requestToken();
-        if (token != null) {
-            System.out.println("Token de autenticação obtido com sucesso: " + request.getToken().getAccess_token());
-        } else {
-            System.out.println("Falha ao obter o token de autenticação.");
+        try {
+            String requestUrl = request.getBaseUrl() + "tracks/6nH5L0CDejkvcMxlO1NLWf";
+            System.out.println(requestUrl);
+            String response = request.sendGetRequest(requestUrl);
+            System.out.println(response);
+        } catch (RequestException e) {
+            System.out.println("Erro ao obter a música: " + e.getMessage());
         }
     }
 }

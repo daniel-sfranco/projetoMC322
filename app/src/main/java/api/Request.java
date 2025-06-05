1b8a902d0bb016bd28ddd211bb3ff24668caea19
daniel-sfranco <daniel.sfranco346@outlook.com>
2025-06-04 21:15:37 -0300

    Mudando .gitignore e removendo classe SpotifyToken do arquivo do request

--- app/src/main/java/api/Request.java
+++ app/src/main/java/api/Request.java
@@ -1,98 +1,9 @@
 package api;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
-
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import exceptions.RequestException;
 
-/**
- * SpotifyToken
- * Classe que representa o token de autenticação retornado pela API do Spotify.
- * Contém os campos access_token, token_type e expires_in.
- */
-class SpotifyToken {
-    private String access_token;
-    private String token_type;
-    private int expires_in;
-    private LocalDateTime updatedAt;
-    private String clientId = "9afeb5fec9854592994aa191f842b529";
-    private String clientSecret = "0e4def4ee8924cb68daba80833c8a5c2"; // Eu juro que vou fazer isso ser mais seguro
-
-    /**
-     * Construtor da classe SpotifyToken.
-     * Inicializa o token de autenticação chamando o método refreshToken().
-     * 
-     * @throws RequestException se ocorrer um erro ao atualizar o token.
-     */
-    public SpotifyToken() throws RequestException {
-        this.refreshToken();
-    }
-
-    /**
-     * Retorna o token de acesso.
-     * 
-     * @return O token de acesso.
-     */
-    public String getAccess_token() {
-        LocalDateTime now = LocalDateTime.now();
-        if (updatedAt == null || updatedAt.plusSeconds(expires_in).isBefore(now)) {
-            try {
-                return this.refreshToken();
-            } catch (Exception e) {
-                System.out.println("Erro ao atualizar o token de autenticação: " + e.getMessage());
-                return null;
-            }
-        }
-        return access_token;
-    }
-
-    /**
-     * Retorna o tipo de token.
-     * 
-     * @return O tipo de token.
-     */
-    public String getToken_type() {
-        return token_type;
-    }
-
-    /**
-     * Retorna o tempo de expiração do token em segundos.
-     * 
-     * @return O tempo de expiração do token.
-     */
-    public int getExpires_in() {
-        return expires_in;
-    }
-
-    /**
-     * Atualiza o token de autenticação.
-     * Faz uma requisição para a API do Spotify para obter um novo token.
-     * 
-     * @return O novo token de acesso ou null em caso de falha.
-     */
-    public String refreshToken() throws RequestException {
-        String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", clientId,
-                clientSecret);
-        Map<String, String> headers = Map.of(
-                "Content-Type", "application/x-www-form-urlencoded");
-        String response;
-        response = HttpClientUtil.sendPostFormRequest(
-                "https://accounts.spotify.com/api/token",
-                headers,
-                body);
-        Map<String, String> params = (Map<String, String>) JsonUtil.parseJson(response,
-                new TypeReference<HashMap<String, String>>() {
-                });
-        this.access_token = params.get("access_token");
-        this.token_type = params.get("token_type");
-        this.expires_in = Integer.parseInt(params.get("expires_in"));
-        this.updatedAt = LocalDateTime.now();
-        return access_token;
-    }
-}
 
 /**
  * Request

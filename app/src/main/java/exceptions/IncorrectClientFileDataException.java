7604121c535a760c6b40152b80313d7a35c2c622
Vinicius de Oliveira Silva <vinyoliveira2005@gmail.com>
2025-06-09 00:18:07 -0300

    Added methods for writing, editing and reading ClientData files

---
+++ app/src/main/java/exceptions/IncorrectClientFileDataException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IncorrectClientFileDataException extends Exception {
+    public IncorrectClientFileDataException(String message) {
+        super(message);
+    }
+}
